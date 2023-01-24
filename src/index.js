2dec34c53ea792234989be7f49d4bdc7d63f4cbc
Samat <s.mukhanov@iitu.edu.kz>
2023-01-25 00:10:10 +0600

    step 3 Creating Todo List

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('Hello Samat');
+import React from "react";
+import ReactDOM from 'react-dom';
+
+//const element = React.createElement('hi', null, 'Hello React World');
+
+const element = (
+    <div>
+        <h1>My Todo List</h1>
+        <input placeholder="search" />
+        <ul>
+            <li>Install React</li>
+            <li>Study React</li>
+            <li>Use React</li>
+            <li>Build React App</li>
+        </ul>
+    </div>
+    );
+
+    ReactDOM.render(element,
+        document.getElementById('root'));
