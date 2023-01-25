e8c90af6d7e71ae09288d15c6ff43a60d85bbb11
Samat <s.mukhanov@iitu.edu.kz>
2023-01-25 07:33:11 +0600

    step 5 JSX

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 
-//const element = React.createElement('hi', null, 'Hello React World');
+
+
+
 const TodoList = () => {
+    const items = ['Install React', 'Study Reac', 'Use React', 'Build React App'];
     return (
         <ul>
-            <li>Install React</li>
-            <li>Study React</li>
-            <li>Use React</li>
-            <li>Build React App</li>
+            <li>{items[0]}</li>
+            <li>{items[1]}</li>
+            <li>{items[2]}</li>
+            <li>{items[3]}</li>
         </ul>
     );
 };
@@ -18,12 +21,30 @@ const AppHeader = () =>{
 };
 
 const SearchPanel = () => {
-    return <input placeholder="search" />;
+
+    const searchText = 'Type here to search';
+    const searchStyle = {
+        fontSize: '20px'
+    };
+
+    // return <input placeholder="search" />;
+    return <input
+        style = {searchStyle}
+        placeholder= {searchText}
+        disabled={true} />;
 }
 
 const App = () => {
+
+    const value = '<script>alert ("")</script>';
+    // const isLoggedIn = false;
+    // const loginBox = <span>Log in to the system</span>;
+    // const welcomeBox = <span>Welcome Back</span>;
+
     return (
         <div>
+            {/*{ isLoggedIn ? null : loginBox }*/}
+            { value }
             <AppHeader />
             <SearchPanel />
             <TodoList/>
