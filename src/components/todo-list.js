bf41efb036ada6d07c1651b00d9f3007b36160c5
Samat <s.mukhanov@iitu.edu.kz>
2023-01-27 00:57:30 +0600

    step 9 Map method for arrays

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -3,28 +3,18 @@ import TodoListItem from "./todo-list-item";
 
 const TodoList = ({ todos}) => {
 
+    const elements = todos.map((item) => {
+        return (
+            <li>
+            <TodoListItem
+                label = {item.label}
+                important={item.important} />
+        </li>
+        );
+    });
     return (
         <ul>
-            <li>
-                <TodoListItem
-                    label = {todos[0].label}
-                    important={todos[0].important} />
-            </li>
-            <li>
-                <TodoListItem
-                    label = {todos[1].label}
-                    important = {todos[1].important} />
-            </li>
-            <li>
-                <TodoListItem
-                    label = {todos[2].label}
-                    important = {todos[2].important} />
-            </li>
-            <li>
-                <TodoListItem
-                    label = {todos[3].label}
-                    important = {todos[3].important} />
-            </li>
+            { elements }
         </ul>
     );
 };
