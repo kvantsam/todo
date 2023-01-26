eb43e2277be60fc7ab175c0a810dbdcb7c805626
Samat <s.mukhanov@iitu.edu.kz>
2023-01-27 00:27:12 +0600

    step 7 Using props in React

--- src/components/todo-list-item.js
+++ src/components/todo-list-item.js
@@ -1,8 +1,13 @@
 import React from "react";
 
-const TodoListItem = () => {
-    return <span>Drink Cofee</span>
+const TodoListItem = ({ label, important = false }) => {
 
+    const style = {
+        color: important ? 'tomato' : 'black'
+    };
+    return <span style={ style }>{ label }</span>
+    //props argument in TodoListItem
+    //return <span>{ props.label }</span>
 };
 
 export default TodoListItem;
--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -5,8 +5,10 @@ const TodoList = () => {
 
     return (
         <ul>
-            <li><TodoListItem /></li>
-            <li><TodoListItem /></li>
+            <li><TodoListItem label = "Drink Coffee" /></li>
+            <li><TodoListItem label = "Implement React Application" /></li>
+            <li><TodoListItem label = "Make notes from your study" /></li>
+            <li><TodoListItem label = "Do not drink alchohol" important /></li>
         </ul>
     );
 };
