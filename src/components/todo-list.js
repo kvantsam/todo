d4576a2d4ee8e3051309aced8b788306ead968e2
Samat <s.mukhanov@iitu.edu.kz>
2023-01-27 00:50:36 +0600

    step 8 Using arrays as components

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,14 +1,30 @@
 import React from "react";
 import TodoListItem from "./todo-list-item";
 
-const TodoList = () => {
+const TodoList = ({ todos}) => {
 
     return (
         <ul>
-            <li><TodoListItem label = "Drink Coffee" /></li>
-            <li><TodoListItem label = "Implement React Application" /></li>
-            <li><TodoListItem label = "Make notes from your study" /></li>
-            <li><TodoListItem label = "Do not drink alchohol" important /></li>
+            <li>
+                <TodoListItem
+                    label = {todos[0].label}
+                    important={todos[0].important} />
+            </li>
+            <li>
+                <TodoListItem
+                    label = {todos[1].label}
+                    important = {todos[1].important} />
+            </li>
+            <li>
+                <TodoListItem
+                    label = {todos[2].label}
+                    important = {todos[2].important} />
+            </li>
+            <li>
+                <TodoListItem
+                    label = {todos[3].label}
+                    important = {todos[3].important} />
+            </li>
         </ul>
     );
 };
--- src/index.js
+++ src/index.js
@@ -5,11 +5,19 @@ import SearchPanel from "./components/search-panel";
 import TodoList from "./components/todo-list";
 
 const App = () => {
+
+    const todoData = [
+        { label: 'Drink Coffee', important: false },
+        { label: 'Implement React Application', important: true },
+        { label: 'Make notes from your stud', important: false },
+        { label: 'Do not drink alchohol', important: false },
+    ];
+
     return (
         <div>
             <AppHeader />
             <SearchPanel />
-            <TodoList/>
+            <TodoList todos =  {todoData} />
         </div>
     );
 }
