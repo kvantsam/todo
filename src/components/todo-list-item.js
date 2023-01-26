615b2b5961c669c7b670dfe31b732d981a054bb5
Samat <s.mukhanov@iitu.edu.kz>
2023-01-27 02:28:05 +0600

    step 12 Bootstrap and CSS importing

--- src/components/todo-list-item.js
+++ src/components/todo-list-item.js
@@ -1,12 +1,14 @@
 import React from "react";
+import './todo-list-item.css';
 
 const TodoListItem = ({ label, important = false }) => {
 
     const style = {
         color: important ? 'tomato' : 'black'
     };
-    return <span style={ style }>{ label }</span>
-
+    return <span className={ "todo-list-item" }
+                 style={ style }> { label }
+    </span>;
 };
 
 export default TodoListItem;
--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,18 +1,20 @@
 import React from "react";
 import TodoListItem from "./todo-list-item";
+import './todo-list.css';
 
 const TodoList = ({ todos}) => {
 
     const elements = todos.map((item) => {
         const { id, ...itemProps } = item;
+
         return (
-            <li key = { item.id } >
+            <li key = {id} className={"list-group-item"} >
             <TodoListItem {...itemProps } />
         </li>
         );
     });
     return (
-        <ul>
+        <ul className={"list-group todo-list"}>
             { elements }
         </ul>
     );
