acadd734528482cec50f612374c98d876fa5f6d3
Samat <s.mukhanov@iitu.edu.kz>
2023-02-09 15:52:48 +0600

    step 19 Use Constructor

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -2,6 +2,14 @@ import React, {Component} from 'react';
 import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
+
+    constructor() {
+        super();
+        this.onLabelClick = () => {
+            console.log(`Done: ${this.props.label}`);
+        };
+    }
+
     render () {
 
         const { label, important = false } = this.props;
@@ -13,7 +21,8 @@ export default class TodoListItem extends Component {
             <span className="todo-list-item">
       <span
           className="todo-list-item-label"
-          style={style}>
+          style={style}
+          onClick={ this.onLabelClick }>
         {label}
       </span>
 
