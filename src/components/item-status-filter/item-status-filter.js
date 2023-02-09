aa83069268737e1c4a18a01a676c0dd33a747b7c
Samat <s.mukhanov@iitu.edu.kz>
2023-02-09 15:37:50 +0600

    step 18 Use stateful component

--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './item-staus-filter.css';
 
-const ItemStatusFilter = () => {
-    return (
-        <div className={"btn-group"}>
-            <button type={"button"}
-                    className={"btn btn-info"}>All list</button>
-            <button type={"button"}
-                    className={"btn btn-outline-secondary"}>Active list</button>
-            <button type={"button"}
-                    className={"btn btn-outline-secondary"}>Done</button>
-        </div>
-    );
-};
-export default ItemStatusFilter;
+export default class ItemStatusFilter extends Component {
+
+    render () {
+        return (
+            <div className={"btn-group"}>
+                <button type={"button"}
+                        className={"btn btn-info"}>All list</button>
+                <button type={"button"}
+                        className={"btn btn-outline-secondary"}>Active list</button>
+                <button type={"button"}
+                        className={"btn btn-outline-secondary"}>Done</button>
+            </div>
+        );
+    }
+}
+
 
