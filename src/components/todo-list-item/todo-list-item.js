1fce649aaf21c6a9346363657588c3858ae6fdc4
Samat <s.mukhanov@iitu.edu.kz>
2023-02-09 15:54:28 +0600

    step 20 Use OnLabelClick Arrow Function

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,12 +3,9 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
-    constructor() {
-        super();
-        this.onLabelClick = () => {
+    onLabelClick = () => {
             console.log(`Done: ${this.props.label}`);
         };
-    }
 
     render () {
 
